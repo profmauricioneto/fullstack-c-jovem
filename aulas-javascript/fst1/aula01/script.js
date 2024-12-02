545d924445039b5c7e22fe8533ffcd2e21849f80
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2024-12-02 16:01:09 -0300

    add: primeira aula de javascript

---
+++ aulas-javascript/fst1/aula01/script.js
@@ -0,0 +1,26 @@
+window.alert("Ola Mundo via JavaScript!");
+console.log("Hello World");
+
+let nameUser = "mauricio";
+let x;
+console.log(typeof nameUser);
+console.log(typeof 19.9);
+console.log(typeof 10);
+console.log(typeof true);
+console.log(typeof []);
+console.log(typeof x);
+
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+// console.log(i);
+
+const PI = 3.1415;
+console.log(PI);
+PI = 4.12;
+
+// {
+//    let nome = 'fulano'; 
+// }
+// nome = 10;
+// console.log(nome);
