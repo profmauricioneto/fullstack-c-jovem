45d47bec530b574e95b338008ce2f8fd68124d62
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2024-12-04 15:30:22 -0300

    aula fst2: javascript - p1

---
+++ aulas-javascript/fst2/hello.js
@@ -0,0 +1,35 @@
+// window.alert("Ola Mundo vindo do JavaScript.");
+
+var a = 10;
+console.log(typeof a);
+console.log(typeof 20.99);
+var b = true;
+console.log(typeof b);
+console.log(typeof "mauricio");
+var c;
+console.log(typeof c);
+b = 'ola'
+console.log(typeof b);
+console.log(typeof []);
+
+let x = 10;
+{
+   // escopo local
+   let nome = "mauricio";
+   nome = "joao"
+   console.log(nome);
+   
+}
+
+const PI = 3.1415;
+console.log(PI);
+
+// console.log(nome);
+
+for(var i = 0; i < 10; i++) {
+    console.log(i);
+}
+console.log("i = " + i);
+
+
+
