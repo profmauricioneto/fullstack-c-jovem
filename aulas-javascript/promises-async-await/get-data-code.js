0f7d595af9673a49b85d1da848c163738b89108f
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2025-01-13 16:57:46 -0300

    add: aula fst1

--- aulas-javascript/promises-async-await/get-data-code.js
+++ aulas-javascript/promises-async-await/get-data-code.js
@@ -1,16 +1,22 @@
 // função que retorna uma Promise para carregar os dados do site do GitHub
 async function getData() {
-    const response = await fetch('https://api.github.com/events');
+    let response;
+    try {
+        response = await fetch('https://api.github.com/events');
+    } catch (err) {
+        console.log(err);
+    }
     const data = await response.json();
     return data;
 }
 
 // chamada da função getData
 getData()
-    .then(data => {
+    .then((data) => {
         console.log(typeof data);
         console.log(data);
     })
-    .catch(error => {
+    .catch((error) => {
         console.error(error);
     });
+
